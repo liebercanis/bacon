27e07e923a8393f71e7ceffa2a072d8ee6706bf4
liebercanis <mgold@unm.edu>
2018-06-21 14:48:41 -0600

    fix rootlogon.C

--- pmt/rootlogon.C
+++ pmt/rootlogon.C
@@ -6,7 +6,7 @@
   printf(" include path %s \n\n",gSystem->GetIncludePath());
   gROOT->LoadMacro("util.C");
   //gROOT->LoadMacro("ntupleRun.C",&iload);
-  int iload = gSystem->Load("$HOME/RnD/bacon/obj/libBaconRoot.so");
+  int iload = gSystem->Load("obj/libBaconRoot.so");
   printf(" loaded libPmtRoot = %i zero is success! \n",iload);
 }
 
